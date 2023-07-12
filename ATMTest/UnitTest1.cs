31fb73780af70c9f320d6fa88ab02041d51517c9
Chyniece Matthews <cfmatthews2@gmail.com>
2023-07-12 18:59:29 -0500

    Lab 2 complete

--- ATMTest/UnitTest1.cs
+++ ATMTest/UnitTest1.cs
@@ -3,16 +3,37 @@ using Microsoft.VisualStudio.TestPlatform.TestHost;
 namespace ATMTest
 {
     public class UnitTest1
+
+
     {
         [Fact]
-        public void Test1()
+        public void ViewBalanceShouldReturnStartingBalance()
         {
-            Assert.Equal(1,1);
+            decimal startingBalance = 0m;
+            decimal currentBalance = Program.ViewBalance();
+
+            Assert.Equal(startingBalance, currentBalance);
         }
-        decimal ViewBalance(decimal value)
+        [Fact]
+        public void DepositShouldIncreaseBalance()
         {
-            Program.Deposit(value);
-            Assert.Equal(value, Program.ViewBalance());
+            decimal depositAmount = 200m;
+            decimal startingBalance = 0m;
+            decimal expectedBalance = startingBalance + depositAmount;
+
+            decimal currentBalance = Program.Deposit(depositAmount);
+
+            Assert.Equal(expectedBalance, currentBalance);
+        }
+        [Fact]
+        public void WithdrawShouldDecreaseBalance() 
+        {
+            decimal withdrawalAmount = 20m;
+            decimal startingBalance = 70m;
+            decimal expectedBalance = startingBalance - withdrawalAmount;
+
+            decimal currentBalance = Program.Withdraw(withdrawalAmount);
+            Assert.Equal(expectedBalance, currentBalance);
         }
     }
 }
--- Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Program.cs
@@ -1,43 +1,80 @@
-﻿namespace Lab02_UnitTesting
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Lab02_UnitTesting
 {
     internal class Program
     {
+       
         private static decimal balance = 0m;
-            public static decimal ViewBalance()
+
+        public static decimal ViewBalance()
         {
             return balance;
         }
 
         public static decimal Withdraw(decimal amount)
+
         {
+            //Console.WriteLine("How much would you like to withdraw?");
+          //decimal withDrawalAmount = decimal.Parse( Console.ReadLine());
+            if (amount < 0|| amount > balance)
+            {
+
+                Console.WriteLine("Please deposit funds.");
+                return balance;
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine("This is your new balance:" + balance);
+                return balance;
+            }
+
+        }
+        public static decimal Deposit(decimal amount)
+        {
+            //Console.WriteLine("How much would you like to deposit?");
+           // decimal depositAmount = decimal.Parse(Console.ReadLine());
             if (amount < 0)
             {
-                return balance;
-            }else
-            {
-                return balance - amount;
+                Console.WriteLine("Invalid amount");
+               
             }
-            
-        }
-        public static decimal Deposit(decimal amount) 
-        {
-            if (amount < 0)
+           else
             {
-                return balance;
-            }else if (amount > 0)
-            {
-                return balance + amount;
+                balance += amount;
+
+                Console.WriteLine("This is your new balance:" + balance);
+                   
             }
-            
+            return balance;
         }
+
         public static void UserInterface()
         {
-           Console.WriteLine(ViewBalance());
-            Console.WriteLine(Deposit(20m));
-            Console.WriteLine(ViewBalance());
-            Console.WriteLine(Withdraw(20m));
-            Console.WriteLine(ViewBalance());
+            Console.WriteLine("Starting Balance:" + ViewBalance());
 
+            Console.WriteLine("How much would you like to deposit?");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            Deposit(depositAmount);
+
+
+            Console.WriteLine("How much would you like to withdraw?:");
+            decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
+            Withdraw(withdrawalAmount);
+
+
+            Console.WriteLine("Here is your final balance:" + ViewBalance());
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+        public static void Main(string[] args)
+        {
+
+            UserInterface();
         }
     }
 }
