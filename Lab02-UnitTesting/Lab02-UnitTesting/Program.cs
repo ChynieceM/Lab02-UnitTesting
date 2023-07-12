53405e979159a48185787f1dd0fd5bbaae736ec0
Chyniece Matthews <cfmatthews2@gmail.com>
2023-07-12 14:40:29 -0500

    lab 2

---
+++ Lab02-UnitTesting/Lab02-UnitTesting/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Lab02_UnitTesting
+{
+    internal class Program
+    {
+        private static decimal balance = 0m;
+            public static decimal ViewBalance()
+        {
+            return balance;
+        }
+
+        public static decimal Withdraw(decimal amount)
+        {
+            if (amount < 0)
+            {
+                return balance;
+            }else
+            {
+                return balance - amount;
+            }
+            
+        }
+        public static decimal Deposit(decimal amount) 
+        {
+            if (amount < 0)
+            {
+                return balance;
+            }else if (amount > 0)
+            {
+                return balance + amount;
+            }
+            
+        }
+        public static void UserInterface()
+        {
+           Console.WriteLine(ViewBalance());
+            Console.WriteLine(Deposit(20m));
+            Console.WriteLine(ViewBalance());
+            Console.WriteLine(Withdraw(20m));
+            Console.WriteLine(ViewBalance());
+
+        }
+    }
+}
